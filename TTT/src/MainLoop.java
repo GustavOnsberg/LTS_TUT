9187f15deaead70d15cb06a51c16c0103379332f
GustavOnsberg <gustavonsberg@gmail.com>
2020-01-20 14:35:27 +0100

    fef

--- TTT/src/MainLoop.java
+++ TTT/src/MainLoop.java
@@ -4,7 +4,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.BigInteger;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.Random;
 import java.net.Socket;
 import java.io.*;
@@ -20,7 +22,7 @@ public class MainLoop extends JFrame implements ActionListener {
     public static PrintWriter pw;
     public static int fromServer;
     private static Object ByteBuffer;
-    static byte[] byteArray = new byte[1024];
+    static byte[] byteArray = new byte[36];
 
     public static void main(String[] args){
         mainLoop = new MainLoop();
@@ -187,11 +189,21 @@ public class MainLoop extends JFrame implements ActionListener {
         Scanner sc2 = new Scanner(System.in);   //Til at tage spillerens input
 
         while (running) {
-            System.out.println("Hi");
             bi.read(byteArray,0,36); //Tager beskeder fra server
-            System.out.println("Hi2");
-            System.out.println(byteArray[3]);
-
+            byte[] b1 = new byte[16];
+            byte[] b2 = new byte[16];
+            for(int i = 0; i < 16; i++){
+                b1[i] = byteArray[i+2];
+            }
+            for(int i = 0; i < 16; i++){
+                b2[i] = byteArray[i+18];
+            }
+            System.out.println(new String(byteArray));
+            BigInteger bigI1 = new BigInteger(b1);
+            System.out.println(bigI1);
+            BigInteger bigI2 = new BigInteger(b2);
+            System.out.println(bigI2);
+            running = false;
 /*
             //Kigger på besked fra server og gør hvad der er passende til situationen
             if(fromServer.contains("YOUR TURN") && false){
